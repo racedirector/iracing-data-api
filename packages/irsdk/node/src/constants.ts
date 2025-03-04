9f6152f43e0bf9aaac777e0667662ffb20353c8f
Justin Makaila <1638996+justinmakaila@users.noreply.github.com>
2025-03-04 11:05:15 -0500

    add types, add basic functionality

---
+++ packages/irsdk/node/src/constants.ts
@@ -0,0 +1,2 @@
+export const SIM_STATUS_URI =
+  "http://127.0.0.1:32034/get_sim_status?object=simStatus";
--- packages/irsdk/node/src/irsdk/index.ts
+++ packages/irsdk/node/src/irsdk/index.ts
@@ -1,3 +1,15 @@
-export class IRacingSDK {}
+import { getSimStatus } from "../utils";
+
+export class IRacingSDK {
+  static async isSimRunning() {
+    try {
+      return await getSimStatus();
+    } catch (e) {
+      console.error(e);
+    }
+
+    return false;
+  }
+}
 
 export default IRacingSDK;
