bb429899da913781623635f06fa66bfd238f155f
Justin Makaila <1638996+justinmakaila@users.noreply.github.com>
2025-03-04 20:40:54 -0500

    Feature/inquirer optional dependency (#24)
    * make inquirer optional in helpers
    
    * bump version
    
    * make sure the functions are the same...

--- packages/helpers/sync-track-assets/src/util.ts
+++ packages/helpers/sync-track-assets/src/util.ts
@@ -1,6 +1,14 @@
 import assert from "node:assert";
 import { access, constants } from "node:fs/promises";
-import inquirer from "inquirer";
+
+async function getInquirer() {
+  try {
+    const inquirerPath = require.resolve("inquirer");
+    return await import(inquirerPath);
+  } catch (_error) {
+    return null;
+  }
+}
 
 /**
  * Checks if a file exists.
@@ -37,31 +45,45 @@ export async function getIRacingCredentials(usernameProp?: string) {
     ? `${process.env.IRACING_PASSWORD}`
     : undefined;
 
-  const { username = usernameOption, password = passwordOption } =
-    await inquirer.prompt([
-      {
-        type: "input",
-        name: "username",
-        message: "Enter your username:",
-        when: () => !usernameOption,
-      },
-      {
-        type: "password",
-        name: "password",
-        message: "Enter your password:",
-        mask: "*",
-        when: () => !passwordOption,
-      },
-    ]);
+  const inquirer = await getInquirer();
+  if (inquirer) {
+    const { username = usernameOption, password = passwordOption } =
+      await inquirer.prompt([
+        {
+          type: "input",
+          name: "username",
+          message: "Enter your username:",
+          when: () => !usernameOption,
+        },
+        {
+          type: "password",
+          name: "password",
+          message: "Enter your password:",
+          mask: "*",
+          when: () => !passwordOption,
+        },
+      ]);
 
-  assert(
-    username,
-    "Please provide username via CLI option, environment variable (IRACING_USERNAME), or enter when prompted."
-  );
-  assert(
-    password,
-    "Please provider password via environment variable (IRACING_PASSWORD) or enter when prompted."
-  );
+    assert(
+      username && username.length > 0,
+      "Could not find username via environment variable (IRACING_USERNAME), please update your env or enter when prompted."
+    );
+    assert(
+      password && password.length > 0,
+      "Could not find password via environment variable (IRACING_PASSWORD), please update your env or enter when prompted."
+    );
 
-  return { username, password };
+    return { username, password };
+  } else {
+    assert(
+      usernameOption && usernameOption.length > 0,
+      "Please provide username via environment variable (IRACING_USERNAME)."
+    );
+    assert(
+      passwordOption && passwordOption.length > 0,
+      "Please provider password via environment variable (IRACING_PASSWORD)."
+    );
+
+    return { username: usernameOption, password: passwordOption };
+  }
 }
