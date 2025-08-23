c8cf70f4cbe82195142aefd6a6a0fb46f82be9d5
Justin Makaila <1638996+justinmakaila@users.noreply.github.com>
2025-08-22 21:54:52 -0400

    Update index.ts

--- examples/oauth-example/src/index.ts
+++ examples/oauth-example/src/index.ts
@@ -80,7 +80,7 @@ app.get("/oauth/iracing/callback", async (req, res, next) => {
 
     const userData = await response.json();
     console.info("User identified as:", userData);
-    res.redirect("/");
+    res.status(200).send()
   } catch (error) {
     next(error);
   }
