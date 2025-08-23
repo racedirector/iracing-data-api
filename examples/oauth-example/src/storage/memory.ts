b027275caad4683899371e3adc298202be2eb0c1
Justin Makaila <1638996+justinmakaila@users.noreply.github.com>
2025-08-22 21:56:10 -0400

    Update memory.ts

--- examples/oauth-example/src/storage/memory.ts
+++ examples/oauth-example/src/storage/memory.ts
@@ -1,10 +1,6 @@
 import { StateStore } from "@iracing-data/oauth-client/dist/schema/state-store";
 
 export class InMemoryStore implements StateStore {
-
-  get(key, options) {
-    return this.state[key];
-  set(key, value) {
   private state = new Map<string, any>();
 
   get(key, options) {
