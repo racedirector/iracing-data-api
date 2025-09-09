38be3993e54e1f8dfd2b94d1c96335a67e604c8b
Justin Makaila <1638996+justinmakaila@users.noreply.github.com>
2025-09-09 13:46:05 -0400

    update codegen scripts

--- packages/iracing-telemetry-types/scripts/codegen.ts
+++ packages/iracing-telemetry-types/scripts/codegen.ts
@@ -1,8 +1,7 @@
 #!/usr/bin/env ts-node
 import path from "node:path";
-// import { downloadSchema } from "./download-schema";
 import { downloadSchema } from "@iracing-data/sync-telemetry-json-schema";
-import { generateTypes } from "./generate-types";
+import { generateTypes } from "@iracing-data/iracing-json-schema-to-typescript";
 
 const API_URL = process.env.API_URL || "localhost:50051";
 
