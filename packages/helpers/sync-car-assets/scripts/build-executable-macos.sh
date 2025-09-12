b77c336ba232748948784ee5f0714be7eb7839d6
Justin Makaila <1638996+justinmakaila@users.noreply.github.com>
2025-09-12 16:00:30 -0400

    add windows build

--- packages/helpers/sync-car-assets/scripts/build-executable-macos.sh
+++ packages/helpers/sync-car-assets/scripts/build-executable-macos.sh
@@ -9,7 +9,7 @@ cp $(command -v node) build/macos/sync-car-assets
 
 codesign --remove-signature build/macos/sync-car-assets
 
-npx postject build/macos/sync-car-assets NODE_SEA_BLOB build/sync-car-assets.blob \
+pnpm dlx postject build/macos/sync-car-assets NODE_SEA_BLOB build/sync-car-assets.blob \
     --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 \
     --macho-segment-name NODE_SEA
 
--- packages/helpers/sync-car-assets/scripts/build-executable-windows.sh
+++ packages/helpers/sync-car-assets/scripts/build-executable-windows.sh
@@ -2,13 +2,14 @@
 
 echo "Building Windows executable"
 
+mkdir -p build/windows
 node --experimental-sea-config sea-config.json
 
-node -e "require('fs').copyFileSync(process.execPath, 'sync-car-assets.exe')"
+node -e "require('fs').copyFileSync(process.execPath, 'build/windows/sync-car-assets.exe')"
 
-signtool remove /s sync-car-assets.exe
+signtool remove /s build/windows/sync-car-assets.exe
 
-npx postject sync-car-assets.exe NODE_SEA_BLOB sea-prep.blob `
+pnpm dlx postject build/windows/sync-car-assets.exe NODE_SEA_BLOB build/sync-car-assets.blob \
     --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
 
-signtool sign /fd SHA256 sync-car-assets.exe
+signtool sign /fd SHA256 build/windows/sync-car-assets.exe
